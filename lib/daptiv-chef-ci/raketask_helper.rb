d52cae9b4cf453e5a404bbbd3cbf852f380bab29
Shawn Neal <sneal@sneal.net>
2013-11-09 08:46:28 -0800

    Bubble up rake task spec failures and don't exit

--- lib/daptiv-chef-ci/raketask_helper.rb
+++ lib/daptiv-chef-ci/raketask_helper.rb
@@ -2,15 +2,33 @@ module DaptivChefCI
   module RakeTaskHelpers
     extend self
     
+    @@exit_on_failure = true
+    
+    def exit_on_failure
+      @@exit_on_failure
+    end
+    
+    def exit_on_failure=(exit_on_failure)
+      @@exit_on_failure = exit_on_failure
+    end
+    
     def execute(&block)
       begin
         block.call()
       rescue SystemExit => ex
-        exit(ex.status)
+        if @@exit_on_failure
+          exit(ex.status)
+        else
+          raise
+        end
       rescue Exception => ex
-        STDERR.puts("#{ex.message} (#{ex.class})")
-        STDERR.puts(ex.backtrace.join("\n"))
-        exit(1)
+        if @@exit_on_failure
+          STDERR.puts("#{ex.message} (#{ex.class})")
+          STDERR.puts(ex.backtrace.join("\n"))
+          exit(1)
+        else
+          raise
+        end
       end
     end
     
--- spec/shared_contexts/rake.rb
+++ spec/shared_contexts/rake.rb
@@ -1,8 +1,12 @@
 require "rake"
+require 'daptiv-chef-ci/raketask_helper'
 
 shared_context "rake" do
   let(:rake)           { create_rake_application() }
   let(:vagrant_driver) { stub() }
+  
+  # let the errors bubble up normally, don't exit the app
+  before { DaptivChefCI::RakeTaskHelpers.exit_on_failure = false }
   
   def create_rake_application
     Rake.application = Rake::Application.new
