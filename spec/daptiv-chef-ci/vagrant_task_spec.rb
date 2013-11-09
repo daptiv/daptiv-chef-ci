3d3325c7f56faa4d9258bdc063bb777cbd00cc72
Shawn Neal <sneal@sneal.net>
2013-11-09 08:59:42 -0800

    Added environment variables support for vagrant task

--- spec/daptiv-chef-ci/vagrant_task_spec.rb
+++ spec/daptiv-chef-ci/vagrant_task_spec.rb
@@ -8,11 +8,26 @@ describe Vagrant::RakeTask, :unit => true do
     it 'should destroy, up, halt, then destroy the box' do
       
       vagrant_driver.should_receive(:destroy).with({ :cmd_timeout_in_seconds => 180, :retry_attempts => 2 })
-      vagrant_driver.should_receive(:up).with({ :cmd_timeout_in_seconds => 7200 })
+      vagrant_driver.should_receive(:up).with({ :cmd_timeout_in_seconds => 7200, :environment => {} })
       vagrant_driver.should_receive(:halt).at_least(1).times.with({ :cmd_timeout_in_seconds => 180, :retry_attempts => 2 })
       vagrant_driver.should_receive(:destroy).with({ :cmd_timeout_in_seconds => 180, :retry_attempts => 2 })
       
       task = rake['vagrant']
+      task.invoke()
+
+    end
+    
+    it 'should up the box with the specified environment vars' do
+
+      environment = { :ENV_VAR1 => 'val1', :ENV_VAR2 => 'val2' }
+      
+      vagrant_driver.should_receive(:destroy).with({ :cmd_timeout_in_seconds => 180, :retry_attempts => 2 })
+      vagrant_driver.should_receive(:up).with({ :cmd_timeout_in_seconds => 7200, :environment => environment })
+      vagrant_driver.should_receive(:halt).at_least(1).times.with({ :cmd_timeout_in_seconds => 180, :retry_attempts => 2 })
+      vagrant_driver.should_receive(:destroy).with({ :cmd_timeout_in_seconds => 180, :retry_attempts => 2 })
+      
+      task = rake['vagrant']
+      subject.environment = environment
       task.invoke()
 
     end
