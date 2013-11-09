9b4bd15264f4f80c42a53163d67be215dc27b964
Shawn Neal <sneal@sneal.net>
2013-11-09 08:53:50 -0800

    Added ability to specify environment variables for vagrant up
    This makes the specified environment variables available to the vagrant up shell command

--- spec/daptiv-chef-ci/vagrant_up_task_spec.rb
+++ spec/daptiv-chef-ci/vagrant_up_task_spec.rb
@@ -7,9 +7,20 @@ describe VagrantUp::RakeTask, :unit => true do
   describe 'vagrant_up' do
     it 'should up the box' do
 
-      vagrant_driver.should_receive(:up).with({ :cmd_timeout_in_seconds => 7200 })
+      vagrant_driver.should_receive(:up).with({ :cmd_timeout_in_seconds => 7200, :environment => {} })
       
       task = rake['vagrant_up']
+      task.invoke()
+
+    end
+    
+    it 'should up the box with the specified environment vars' do
+
+      environment = { :ENV_VAR1 => 'val1', :ENV_VAR2 => 'val2' }
+      vagrant_driver.should_receive(:up).with({ :cmd_timeout_in_seconds => 7200, :environment => environment })
+      
+      task = rake['vagrant_up']
+      subject.environment = environment
       task.invoke()
 
     end
